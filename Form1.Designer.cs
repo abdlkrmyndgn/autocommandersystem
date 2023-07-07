7fe91f6541a6195e294e9ab80cb5597abb976ae3
Abdulkerim Yenidoğan <76014510+abdlkrmyndgn@users.noreply.github.com>
2023-07-07 14:04:30 +0300

    Add files via upload
    Son Güncekkeme

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,13 +38,17 @@ namespace autocommandersystem
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.panel1 = new System.Windows.Forms.Panel();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
             this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
-            this.progressBar1.Location = new System.Drawing.Point(665, 12);
+            this.progressBar1.Location = new System.Drawing.Point(1106, 12);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.progressBar1.Size = new System.Drawing.Size(68, 23);
@@ -55,25 +59,27 @@ namespace autocommandersystem
             // 
             this.button1.BackColor = System.Drawing.Color.Lime;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
-            this.button1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(242, 359);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(58, 312);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 44);
+            this.button1.Size = new System.Drawing.Size(103, 48);
             this.button1.TabIndex = 1;
             this.button1.Text = "ÇALIŞTIR";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Brown;
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
-            this.button2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(429, 359);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(58, 379);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 44);
+            this.button2.Size = new System.Drawing.Size(103, 50);
             this.button2.TabIndex = 2;
             this.button2.Text = "DURDUR";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // radioButton1
             // 
@@ -96,6 +102,7 @@ namespace autocommandersystem
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Dış Aydınlatma";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
@@ -110,7 +117,7 @@ namespace autocommandersystem
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.BackColor = System.Drawing.Color.NavajoWhite;
             this.panel1.Controls.Add(this.radioButton4);
             this.panel1.Controls.Add(this.radioButton1);
             this.panel1.Controls.Add(this.radioButton3);
@@ -130,6 +137,35 @@ namespace autocommandersystem
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Aydınlatmalar Kapalı";
             this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(956, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(1019, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Bilecik 29 °C";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(234, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(923, 514);
+            this.panel2.TabIndex = 9;
             // 
             // Form1
             // 
@@ -137,20 +173,26 @@ namespace autocommandersystem
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(745, 436);
+            this.ClientSize = new System.Drawing.Size(1186, 601);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Commander System";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -164,6 +206,9 @@ namespace autocommandersystem
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,205 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Management;
 
 namespace autocommandersystem
 {
     public partial class Form1 : Form
     {
+        SerialPort serialPort;
+        PromptBuilder asistan = new PromptBuilder();//Bilgisayarımızın Bize Sesli Dönüş yapması için kullanılır.
+        SpeechSynthesizer synt = new SpeechSynthesizer();
+
         public Form1()
         {
             InitializeComponent();
+            serialPort = new SerialPort("COM5", 9600);
+            try
+            {
+                serialPort.Open();//Seri haberleşme birimlerini etkin hale getirdim
+            }
+            catch
+            {
+                Console.WriteLine("Port Açılamadı Port aktif deil.");
+            }
+        }
+
+        public class WeatherData
+        {
+            public MainData Main { get; set; }
+        }
+
+        public class MainData
+        {
+            public float Temp { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Çalıştırma Butonu
+        {
+            asistanSpeak("Engine is Starting");
+            asistanSpeak("Three");
+            Thread.Sleep(1000);
+            asistanSpeak("Two");
+            Thread.Sleep(1000);
+            asistanSpeak("One");
+            if (serialPort.IsOpen)
+            {
+                serialPort.Write("1");
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//Durdurma Butonu
+        {
+            if(serialPort.IsOpen)
+            {
+                serialPort.Write("0");
+            }
+            asistanSpeak("Engine Stop");
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(serialPort.IsOpen)
+            {
+                serialPort.Write("0");
+                serialPort.Close();
+            }
+        }
+
+        public void asistanSpeak(string text)
+        {
+            synt.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen);
+            asistan.ClearContent();
+            asistan.AppendText(text);
+            synt.Speak(asistan);
+        }
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr child, IntPtr newParent);
+
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        private const int WM_SYSCOMMAND = 274;
+
+        private const int SC_MAXIMIZE = 61488;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            asistanSpeak("WellCome To AutoCommander, Pleas Wait Programing is starting...");
+            pictureBox1.BackColor = Color.Transparent;
+            label1.BackColor = Color.Transparent;
+            string exeyolu = "C:\\Program Files (x86)\\Mission Planner\\MissionPlanner.exe";
+            Process calistir = Process.Start(exeyolu);
+
+            while (calistir.MainWindowHandle == IntPtr.Zero || !IsWindowVisible(calistir.MainWindowHandle))
+            {
+                System.Threading.Thread.Sleep(10);
+                calistir.Refresh();
+            }
+            System.Threading.Thread.Sleep(100);
+            //calistir.WaitForInputIdle();
+            SetParent(calistir.MainWindowHandle, this.panel2.Handle);
+            SendMessage(calistir.MainWindowHandle, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
+
+            //Hava Durumu Verilerini Alma//
+            async System.Threading.Tasks.Task Main(string[] args)
+            {
+                string apiKey = "27c907479028a96b0ce70e2fc17cfeb3"; //HAVA DURUMU API TOKENİM
+                string city = "Bilecik";
+
+                HttpClient client = new HttpClient();
+
+                try
+                {
+                    string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+                    string json = await client.GetStringAsync(url);
+
+                    WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(json);
+
+                    label1.Text = city + weatherData.Main.Temp;
+                   
+                }
+                catch (Exception ex)
+                {
+                    for (; ; )
+                    {
+                        asistanSpeak("Weather data is not received please stop the vehicle.");//Hava Durumu verileri alınamadı
+                    }
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+            }
+            sarjverilerinial();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+          
+            if (radioButton2.Checked||radioButton3.Checked||radioButton1.Checked)
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write("2");
+                }
+                asistanSpeak("Lighting is Open");
+            }
+        }
+
+        void sarjverilerinial()//Windows kaynak izleme kullanılarak sarj verileri alınmıştır.
+        {
+            try
+            {
+                ManagementScope scope = new ManagementScope("\\\\.\\ROOT\\CIMV2");
+                ObjectQuery query = new ObjectQuery("SELECT EstimatedChargeRemaining, BatteryStatus FROM Win32_Battery");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+                ManagementObjectCollection collection = searcher.Get();
+
+                foreach (ManagementObject obj in collection)
+                {
+                    int chargeRemaining = Convert.ToInt32(obj["EstimatedChargeRemaining"]);
+                    progressBar1.Value = chargeRemaining;
+                }
+            }
+            catch (Exception ex)
+            {
+                for (; ; )
+                {
+                    asistanSpeak("charging data is not received please stop the vehicle.");//Sarj verileri alınamadı.
+                }
+                
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton4.Checked)
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write("0");
+                }
+                asistanSpeak("Lighting is Close");
+            }
+            
         }
     }
 }
